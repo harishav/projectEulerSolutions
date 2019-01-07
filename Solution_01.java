dfbcf1111e320a2eff6e63f0d501274f59fdea0d
harishav <Harish.akuluru@outlook.com>
2019-01-07 19:41:46 +0530

    Modified solution...
    Added a method to handle the common multiplications....

--- Solution_01.java
+++ Solution_01.java
@@ -4,18 +4,16 @@ public class Solution_01 {
 		
 		int n = 1000;
 		
-		int maxThreeDiv = (n-1)/3;
-		int maxFiveDiv = (n-1)/5;
-		int maxFifteenDiv = (n-1)/15;
-		
-		long sumOfThreeDiv = 3*(maxThreeDiv*(maxThreeDiv+1)/2);
-		long sumOfFiveDiv = 5*(maxFiveDiv*(maxFiveDiv+1)/2);
-		long sumOfFifteenDiv = 15*(maxFifteenDiv*(maxFifteenDiv+1)/2);
-		
-		long finalSum = sumOfThreeDiv + sumOfFiveDiv - sumOfFifteenDiv;
-		
+		long finalSum = sumOfNumbers(n-1,3) + sumOfNumbers(n-1,5) - sumOfNumbers(n-1,3*5);
 
 		System.out.println(finalSum);
+	}
+	
+	private static long sumOfNumbers(int limit, int div) {
+		limit = limit/div; //sum numbers <= n that are divisible by d
+		long sum = div*(limit*(limit+1)/2); // d * n(n+1)/2 -- number * sum of 1st n digits
+		
+		return sum;
 	}
 		    		
 }
